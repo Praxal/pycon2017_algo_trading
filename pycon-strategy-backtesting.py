fc3135f0c8e47f79c22150803bf31dd8b8e17dc1
guanidene <guanidene@gmail.com>
2017-08-21 23:17:54 +0530

    Improved strategy algo and Added plotting feature

--- pycon-strategy-backtesting.py
+++ pycon-strategy-backtesting.py
@@ -57,12 +57,31 @@ class AlgoTrading:
         self.buy_trades = []
         self.sell_trades = []
         self.collect_data()
-        self.initialize_crossover1()
-        self.initialize_crossover2()
+        self.initialize_crossover()
+        self.trade_history = []
+        self.qty = 0
+        self.previous_profit_percent = 0
 
     def collect_data(self):
         self.csv = pandas.read_csv(PATH_DATA_POINTS)
+        self.dates = [dt.datetime.strptime(date,'%d/%m/%y %H:%M') for date in self.csv['Date']][::-1][:self.sampling_points]
         self.data_points = self.csv['TATASTEEL-EQ C'].tolist()[::-1][:self.sampling_points]
+
+    def update_trade_history(self, stock_price, trade_buy=False, trade_sell=False, profit_percent=-1):
+        "Should be called on every data point processed"
+        if trade_buy:
+            self.qty += 1
+            pp = profit_percent
+        elif trade_sell:
+            self.qty -= 1
+            pp = profit_percent
+        else:
+            pp = self.previous_profit_percent
+        self.trade_history.append({'stock_price': stock_price,
+                                   'quantity': self.qty,
+                                   'profit_percent': pp
+                                    })
+        self.previous_profit_percent = pp
 
     def sma(self, data, window):
         """
@@ -82,93 +101,91 @@ class AlgoTrading:
             current_ema = (c * value) + ((1 - c) * current_ema)
         return current_ema
 
-    def initialize_crossover1(self):
+    def initialize_crossover(self):
         self.prev_val1 = 0
         self.prev_val2 = 0
 
-    def crossover1(self, val1, val2):
+    def crossover(self, val1, val2):
         cmp1 = cmp(val1, val2)
         cmp2 = cmp(self.prev_val1, self.prev_val2)
         if (not (self.prev_val1 == 0 and self.prev_val2 == 0)):         # don't trigger crossover when called first time
             self.prev_val1, self.prev_val2 = val1, val2
             if cmp1 > cmp2:
-                return True
+                return 1
+            elif cmp1 < cmp2:
+                return -1
             else:
-                return False
+                return 0
         else:
             self.prev_val1, self.prev_val2 = val1, val2
-            return False
+            return 0
 
-    def initialize_crossover2(self):
-        self.prev_val3 = 0
-        self.prev_val4 = 0
-
-    def crossover2(self, val3, val4):
-        cmp1 = cmp(val3, val4)
-        cmp2 = cmp(self.prev_val3, self.prev_val4)
-        if (not (self.prev_val3 == 0 and self.prev_val4 == 0)):         # don't trigger crossover when called first time
-            self.prev_val3, self.prev_val4 = val3, val4
-            if cmp1 > cmp2:
-                return True
-            else:
-                return False
-        else:
-            self.prev_val3, self.prev_val4 = val3, val4
-            return False
-
-    def trade(self):
-        for i,data in enumerate(self.data_points):
-            if i >=29:      # ema(15) needs atleast 30 points
-                # Buy crossover
-                if self.crossover1(self.ema(self.data_points[:i+1], 3), self.ema(self.data_points[:i+1], 15)):
+    def strategy1(self):
+        for i, data in enumerate(self.data_points):
+            crossover = 0
+            profit_percent = 0
+            if i >= 29:      # ema(15) needs atleast 30 points
+                crossover = self.crossover(self.ema(self.data_points[:i+1], 3), self.ema(self.data_points[:i+1], 15))
+                if (crossover == 1):
+                    # Buy crossover
                     print "Buy (Value: %.2f)" % data
                     self.buy_trades.append(data)
-
-                # Sell crossover
-                if self.crossover2(self.ema(self.data_points[:i+1], 15), self.ema(self.data_points[:i+1], 3)):
+                elif (crossover == -1):
+                    # Sell crossover
                     print "Sell (Value: %.2f)" % data
                     self.sell_trades.append(data)
 
-#        print 'Final numbers:'
-        buy_qty = len(self.buy_trades)
-        sell_qty = len(self.sell_trades)
-        qty = min(buy_qty, sell_qty)
-        sell = sum(self.sell_trades[:qty])
-        buy = sum(self.buy_trades[:qty])
-        profit = sell - buy
-        profit_percent = profit*100.0/(buy/len(self.buy_trades))
-        print 'Buy_qty:%s, Sell qty:%s, Min qty:%s' %(buy_qty, sell_qty, qty)
-        print 'Buy trades:', [("%.2f" % trade) for trade in self.buy_trades[:qty]]
-        print 'Sell trades:', [("%.2f" % trade) for trade in self.sell_trades[:qty]]
-        print 'Total buy: %s' % buy
-        print 'Total sell: %s' % sell
-        print 'Total profit: %s' % profit
-        print 'Total profit_percent: %s' % profit_percent
+                # If crossover happens...
+                if (crossover is not 0):
+                    buy_qty = len(self.buy_trades)
+                    sell_qty = len(self.sell_trades)
+                    qty = min(buy_qty, sell_qty)
+                    if qty:
+                        sell = sum(self.sell_trades[:qty])
+                        buy = sum(self.buy_trades[:qty])
+                        profit = sell - buy
+                        profit_percent = profit*100.0/(buy/len(self.buy_trades))
+    #                    self.profit_percents.append(profit_percent)
+    #                print 'Buy_qty:%s, Sell qty:%s, Min qty:%s' %(buy_qty, sell_qty, qty)
+    #                print 'Buy trades:', [("%.2f" % trade) for trade in self.buy_trades[:qty]]
+    #                print 'Sell trades:', [("%.2f" % trade) for trade in self.sell_trades[:qty]]
+    #                print 'Total buy: %s' % buy
+    #                print 'Total sell: %s' % sell
+    #                print 'Total profit: %s' % profit
+    #                print 'Total profit_percent: %s' % profit_percent
+                        yield profit_percent
 
-        return profit_percent
+            # update history
+            self.update_trade_history(data, crossover == 1, crossover == -1, profit_percent)
+
+    def trade(self):
+        for profit_percent in self.strategy1():
+            print profit_percent
+
+    def plot(self):
+        df = pandas.DataFrame(self.trade_history)
+
+        plt.ion()
+        plt.figure(1)
+        plt.hold(True)
+
+        for i, row in df.iterrows():
+            #            plt.subplot(211)
+            #            plt.plot(self.dates, df['stock_price'])
+            #            plt.subplot(212)
+            #            plt.plot(self.dates, df['profit_percent'])
+
+            print row
+            plt.subplot(211)
+            plt.plot(self.dates[i], row['stock_price'], color='b', marker='_')
+            plt.subplot(212)
+            plt.plot(self.dates[i], row['profit_percent'], color='b', marker='_')
+            plt.pause(0.01)
+
+
 
 
 if __name__ == "__main__":
-    #    import sys
-    #no = int(sys.argv[1])
-    #    p = plot_algotrading(1125)
-
-    
     algotrading = AlgoTrading()
-    print algotrading.trade()
-    raise SystemExit
-    y = []
-    x = xrange(101,15001, 100)
-    for x_ in x:
-        algotrading = AlgoTrading(x_)
-        profit = algotrading.trade()
-        y.append(profit)
-        print profit
-    
-    plt.plot(x,y)
-    plt.show()
-
-
-#    while True:
-#        time.sleep(100)
-
+    algotrading.trade()
+    algotrading.plot()
